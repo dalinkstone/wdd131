cab849092bdbfbe70c26a5ce0edeefbd39726a7e
dalinkstone <95542331+dalinkstone@users.noreply.github.com>
2025-05-17 22:41:55 -0600

    week4 assignment part 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,9 +1,41 @@
-const menuSelector = document.querySelector("button");
-
+const menuButton = document.querySelector(".menu");
 function displayMenu() {
+    const dropdown = document.querySelector(".dropdown")
+    dropdown.classList.toggle("hide")    
+}
+menuButton.addEventListener('click', displayMenu);
 
-    document.querySelector('.dropdown').setAttribute('display', 'block');
-    
+const dialog = document.createElement('dialog');
+const gallery = document.querySelector('.gallery');
+document.querySelector('main').prepend(dialog);
+
+function displayModal(event) {
+    const clicked = event.target.closest('img');
+    const src = clicked.src;
+    const alt = clicked.alt;
+
+    const finalSrc = src.split('-')[0] + '-full.jpeg';
+
+    dialog.innerHTML = `<img src="${finalSrc}" alt="${alt}"><button class="close-viewer">X</button>`;
+
+    dialog.showModal();
 }
 
-menuSelector.addEventListener('mouseover', displayMenu);
+function closeButton() {
+    dialog.close();
+}
+
+gallery.addEventListener('click', displayModal);
+
+dialog.addEventListener('click', (event) => {
+    if (event.target === dialog) {
+        dialog.close();
+    }
+})
+
+dialog.addEventListener('click', (event) => {
+    const close = event.target.closest('.close-viewer');
+    if (close) {
+        dialog.close();
+    }
+})
